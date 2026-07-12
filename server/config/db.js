e4dbadfa260d11dc4380ba42df27dd454bd7cf63
Supriya Malakar <supriyamalakarind@gmail.com>
2026-07-13 02:01:06 +0530

    Updated database configuration for Railway

--- server/config/db.js
+++ server/config/db.js
@@ -1,19 +1,21 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Malakar@123",
-  database: "bellas_bouquet",
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
 });
 
 db.connect((err) => {
   if (err) {
-    console.error("Database connection failed:", err);
+    console.error("❌ Database connection failed:");
+    console.error(err);
     return;
   }
 
-  console.log("✅ Connected to MySQL Database");
+  console.log("✅ Connected to Railway MySQL Database");
 });
 
 module.exports = db;
